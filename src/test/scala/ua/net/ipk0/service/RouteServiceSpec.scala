a52e68fe5c0210de49e84f2cf35fb9d89590e409
ivan.puchko <ivan.puchko@gmail.com>
2024-03-11 00:50:26 +0200

    added a few tests

--- src/test/scala/ua/net/ipk0/service/RouteServiceSpec.scala
+++ src/test/scala/ua/net/ipk0/service/RouteServiceSpec.scala
@@ -24,4 +24,29 @@ class RouteServiceSpec extends FlatSpec with Matchers with ScalatestRouteTest wi
       responseAs[String].parseJson shouldBe "[{\"indexes\":[1,2],\"values\":[8,10]}]".parseJson
     }
   }
+
+  it should "return BadRequest within array size limitation" in {
+    Post("/find", Input(Array(3), Some(18))) ~> testRoute ~> check {
+      status shouldEqual StatusCodes.BadRequest
+      responseAs[String] shouldBe "The length of the array must be greater than 1 and less than 10000"
+    }
+  }
+
+  it should "return ToManyRequest within throttling feature" in {
+    val targetOps = Some(17)
+    val clientMock = mock[HttpBinServiceClient]
+    when(clientMock.getTargetValue(targetOps)).thenReturn(Future.successful(targetOps.value))
+
+    val throttleLimit = 5
+    val testRoute = new RouteService(clientMock, new NumberFinderService(), throttleLimit).route
+
+    (1 to throttleLimit).foreach { _ =>
+      Post("/find", Input(Array(3, 5), Some(17))) ~> testRoute
+    }
+
+    Post("/find", Input(Array(3, 5), Some(17))) ~> testRoute ~> check {
+      status shouldEqual StatusCodes.TooManyRequests
+      responseAs[String] shouldBe "The user has sent too many requests in a given amount of time."
+    }
+  }
 }
